c994e5723b6c8b1dcebe5f37f32f3be402271ac9
Leo Maroni <me@leomaroni.de>
2019-04-16 11:02:19 +0200

    [FEAT] Made header responsive; sorting scss; npm audit

--- app/src/assets/js/app.js
+++ app/src/assets/js/app.js
@@ -1,1 +1,8 @@
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+import $ from 'jquery'
+// NAVBAR MOBILE COLLAPSE
+$(document).ready(function() {
+    $(".navbar-burger").click(function() {
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
+});
