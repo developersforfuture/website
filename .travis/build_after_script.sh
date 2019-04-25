02e55b050e214a6e70171a541243fe5098e898b9
Maximilian Berghoff <maximilian.berghoff@gmx.de>
2019-04-25 19:42:45 +0000

    Fix pipeline get it running

--- .travis/build_after_script.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "+ + + Run build_after_script.sh + + +"
-docker logout ${REGISTRY}
--- .travis/test_install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "+ + + Run test_install.sh + + +"
-sudo apt update
-sudo apt install -y python3 m4 curl git make
-pip3 install docker-compose
